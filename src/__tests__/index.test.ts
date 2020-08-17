30dbadad4c06b6f437054051f51fde0719fd3b8f
Vincent Guillou <vin.guillou@gmail.com>
2020-08-17 23:33:51 +0200

    Tests with it.each

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,9 +2,15 @@ import getDateString from '@src/index'
 
 describe('index', () => {
   describe('simple cases', () => {
-    it('should return the date as string', async () => {
-      const result = getDateString(new Date('2020/04/18'))
-      expect(result).toBe('2020-04-18')
+    it.each`
+      date                      | expected
+      ${new Date('2020/08/17')} | ${'2020-08-17'}
+      ${new Date('2020/12/31')} | ${'2020-12-31'}
+      ${new Date('2021/01/01')} | ${'2021-01-01'}
+      ${new Date('2024/02/29')} | ${'2024-02-29'}
+    `("should return string '$expected' for date $date", ({ date, expected }) => {
+      const result = getDateString(date)
+      expect(result).toBe(expected)
     })
   })
 })
